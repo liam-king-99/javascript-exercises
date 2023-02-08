36898f5109b5f99b2563214b79246c33ea83302f
Liam King <Liamking84@gmail.com>
2023-02-08 01:16:33 -0500

    Complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
-
+const reverseString = function(stringToReverse) {
+    let finalString = ''
+    let index = stringToReverse.length
+    while (index >= 0)
+    {
+        finalString = finalString.concat(stringToReverse.charAt(index))
+        index--
+    }
+    return finalString
 };
+
+console.log(reverseString('Hello there'))
 
 // Do not edit below this line
 module.exports = reverseString;
