a4b946f5261a98e986b47832491eda89049da908
liam-king-99 <liamking84@gmail.com>
2023-02-08 12:16:20 -0500

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    if (start < 0 || end < 0)
+    {
+        return 'ERROR'
+    }
+    if (typeof(start) !== 'number' || typeof(end) !== 'number')
+    {
+        return 'ERROR'
+    }
+    let sum = 0
+    let lowerNumber = Math.min(start, end)
+    let higherNumber = Math.max(start, end)
+    for (let i = lowerNumber; i <= higherNumber; i++)
+    {
+        sum += i
+    }
+    return sum
 };
 
 // Do not edit below this line
