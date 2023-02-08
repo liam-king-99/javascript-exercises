f7e1ab1567ad3ee1412c642c99351294edf8566c
Liam King <Liamking84@gmail.com>
2023-02-08 01:05:49 -0500

    Complete exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(stringToRepeat, numberOfTimesToRepeatString) {
+    let finalString = ''
+    let i = 0
+    while (i < numberOfTimesToRepeatString)
+    {
+        finalString = finalString.concat(stringToRepeat)
+        i++
+    }
+    return finalString
 };
 
 // Do not edit below this line
