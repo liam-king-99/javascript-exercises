16fa6def481d20d47148bb031d9dd46c4ee27da1
Liam King <Liamking84@gmail.com>
2023-02-14 13:08:28 -0500

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    num = Number(num)
+    if (num < 0)
+    {
+        return "OOPS"
+    }
+    let low = 0
+    let high = 1
+    while (num > 0)
+    {
+        temp = high
+        high += low
+        low = temp
+        num -= 1
+    }
+    return low
 
 };
 
