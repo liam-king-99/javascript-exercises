af398e02608d3921efe37e307bf6bad6484f19ce
Liam King <Liamking84@gmail.com>
2023-02-14 13:49:30 -0500

    Complete exercises 11 and 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((oldest, currentPerson) => {
+        const oldestAge = getAge(oldest)
+        const currentPersonAge = getAge(currentPerson)
+        return oldestAge > currentPersonAge ? oldest : currentPerson
+    })
 };
+
+const getAge = (person) => {
+    if (!person.yearOfDeath)
+    {
+        return (new Date().getFullYear()) - person.yearOfBirth
+    }
+    return person.yearOfDeath - person.yearOfBirth
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
